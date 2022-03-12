045db6c28536c8a04b137b8c3f7106d5269191c8
mesaque <mesaque1212@gmail.com>
2022-03-12 08:55:35 -0300

    Criando as rotas posts.store e posts.create

--- routes/web.php
+++ routes/web.php
@@ -3,8 +3,9 @@
 use App\Http\Controllers\PostController;
 use Illuminate\Support\Facades\Route;
 
- route::get('/posts' , [PostController::class , 'index']);
-
+route::POST('/posts' , [PostController::class , 'store'])->name('posts.store');
+route::GET('/posts/create' , [PostController::class , 'create'])->name('posts.create');
+route::GET('/posts' , [PostController::class , 'index'])->name('posts.index');
 
 Route::get('/', function () {
     return view('welcome');
