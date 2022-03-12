b36d4ad0c8417bf31555930411a5e31e2a76c993
mesaque <mesaque1212@gmail.com>
2022-03-12 08:53:52 -0300

    Adicionando function create e store

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -9,9 +9,20 @@ class PostController extends Controller
 {
     public function index()
     {
-
         $posts = Post::get();
 
         return view('admin/Posts/index' , compact('posts'));
+    }
+
+    public function create()
+    {
+        return view('admin/Posts/create');
+    }
+
+    public function store(Request $request)
+    {
+        Post::create($request->all());
+
+        return redirect()->route('posts.index');
     }
 }
