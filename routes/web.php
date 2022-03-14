fa6925f2f9bd77ebfb987f1b4d6d0896c505c0b2
mesaque <mesaque1212@gmail.com>
2022-03-14 15:47:18 -0300

    Criando as rotas do show e destroy

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,8 @@
 use App\Http\Controllers\PostController;
 use Illuminate\Support\Facades\Route;
 
+route::delete('/posts/{id}' , [PostController::class , 'destroy'])->name('posts.destroy');
+route::GET('/posts/{id}' , [PostController::class , 'show'])->name('posts.show');
 route::POST('/posts' , [PostController::class , 'store'])->name('posts.store');
 route::GET('/posts/create' , [PostController::class , 'create'])->name('posts.create');
 route::GET('/posts' , [PostController::class , 'index'])->name('posts.index');
