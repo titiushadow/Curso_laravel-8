920167471432173834565e7302413da50af31062
mesaque <mesaque1212@gmail.com>
2022-03-14 15:46:29 -0300

    Colocando a mensagem de confirmação quando o post for deletado

--- resources/views/admin/Posts/index.blade.php
+++ resources/views/admin/Posts/index.blade.php
@@ -1,8 +1,13 @@
 <h1>Posts</h1>
 
+@if(session('message'))
+<div>
+    {{ session('message') }}
+</div>
+@endif
+
 @foreach ($posts as $Post )
-    <p>{{$Post->title}}</p>
-    <p>{{$Post->content}}</p>
+    <p>{{$Post->title}} [<a href="{{ route('posts.show' , $Post -> id)  }}">Ver]</a></p>
 @endforeach
 
 <button>
