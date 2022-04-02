b2fdbd6879e30e0d5e33f82515bae5a6bb9dbe5f
mesaque <mesaque1212@gmail.com>
2022-04-02 12:42:13 -0300

    Adicionando a rota edit e update

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,11 @@
 use App\Http\Controllers\PostController;
 use Illuminate\Support\Facades\Route;
 
+
+
+route::put('/posts/{id}', [PostController::class, 'update'])->name('posts.update');
 route::post('/posts/search' , [PostController::class, 'search'])->name('posts.search');
+route::get('/posts/edit/{id}', [PostController::class, 'edit'])->name('posts.edit');
 route::delete('/posts/{id}' , [PostController::class , 'destroy'])->name('posts.destroy');
 route::POST('/posts/{id}' , [PostController::class , 'show'])->name('posts.show');
 route::POST('/posts' , [PostController::class , 'store'])->name('posts.store');
