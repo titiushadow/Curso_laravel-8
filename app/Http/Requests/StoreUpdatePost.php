3bf56ef3631ecbbb2745c64204585bcbeb11b365
mesaque <mesaque1212@gmail.com>
2022-04-06 21:09:55 -0300

    Adicionando as regas

--- app/Http/Requests/StoreUpdatePost.php
+++ app/Http/Requests/StoreUpdatePost.php
@@ -2,7 +2,10 @@
 
 namespace App\Http\Requests;
 
+
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
+
 
 class StoreUpdatePost extends FormRequest
 {
@@ -23,10 +26,24 @@ class StoreUpdatePost extends FormRequest
      */
     public function rules()
     {
-        return [
-            'title' => 'required|min:3|max:160',
-            'content' => ['nullable| min:5| max:255'],
-            'image' => ['required', 'image']
-        ];
+        $id = $this->segment(3);
+
+        $rules = [
+            'title' => [
+                'required',
+                'min:3',
+                'max:160',
+
+                Rule::unique('posts')->ignore($id),
+                 ],
+                'content' => ['nullable| min:5| max:255'],
+                'image' => ['required', 'image']
+            ];
+
+            if($this->method() == 'PUT') {
+                $rules['image'] = ['nullable', 'image'];
+            }
+
+        return $rules;
     }
 }
