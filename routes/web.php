40bf0b3d6e6e108724b56581c11f067c9c75b6cc
mesaque <mesaque1212@gmail.com>
2022-04-06 21:20:02 -0300

    scaffolding auth

--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,12 @@ route::POST('/posts' , [PostController::class , 'store'])->name('posts.store');
 route::GET('/Posts/create' , [PostController::class , 'create'])->name('posts.create');
 route::GET('/posts' , [PostController::class , 'index'])->name('posts.index');
 
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth'])->name('dashboard');
+
 Route::get('/', function () {
     return view('welcome');
 });
+
+require __DIR__.'/auth.php';
